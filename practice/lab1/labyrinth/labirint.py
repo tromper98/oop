61be77fc0c1566e65bef44b9b42e0e540ff9e296
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-01-30 22:56:15 +0300

    Добавил методы Labyrinth.to_file, Cell.get_cell

--- practice/lab1/labyrinth/labirint.py
+++ practice/lab1/labyrinth/labirint.py
@@ -4,6 +4,7 @@ BORDER = '#'
 PASSABLE = ' '
 START = 'S'
 FINISH = 'F'
+ROOT = '_'
 
 
 class Cell:
@@ -25,17 +26,41 @@ class Cell:
     def is_passable(self) -> bool:
         return True if self._cell_type == PASSABLE else False
 
-    def get_coords(self) -> Tuple[int, int]:
+    def is_root(self) -> bool:
+        return True if self._cell_type == ROOT else False
+
+    def get_coordinates(self) -> Tuple[int, int]:
         return self._x, self._y
 
     def set_distance(self, distance) -> None:
         self.distance = distance
 
+    def get_cell(self) -> str:
+        if self.is_border():
+            return BORDER
+
+        if self.is_start():
+            return START
+
+        if self.is_finish():
+            return FINISH
+
+        if self.is_passable():
+            return PASSABLE
+
+        return ROOT
+
 
 class Labyrinth:
     def __init__(self, field: List[List[Cell]]):
-        self.field = field
+        self.field: List[List[Cell]] = field
 
+    def to_file(self, output_file_path: str):
+        with open(output_file_path, 'w', encoding='utf-8') as file:
+            for row in self.field:
+                for cell in row:
+                    file.write(cell.get_cell())
+                file.write('\n')
 
     @staticmethod
     def from_file(filename: str):
@@ -48,3 +73,4 @@ class Labyrinth:
                     field_row.append(cell)
                 field.append(field_row)
         return Labyrinth(field)
+
