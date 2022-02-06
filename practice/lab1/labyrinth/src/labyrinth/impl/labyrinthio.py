6c4d4f02a597190057d201be370e08b4b7decf79
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-02-06 23:04:57 +0300

    Добавил получение abspath для файлов

--- practice/lab1/labyrinth/src/labyrinth/impl/labyrinthio.py
+++ practice/lab1/labyrinth/src/labyrinth/impl/labyrinthio.py
@@ -1,3 +1,4 @@
+import os.path as path
 from typing import List
 
 from .cell import Cell
@@ -6,16 +7,16 @@ from .cell import Cell
 class LabyrinthIO:
     @staticmethod
     def save_to_file(field: List[List[Cell]], output_file_path: str):
-        with open(output_file_path, 'w', encoding='utf-8') as file:
+        with open(path.abspath(output_file_path), 'w', encoding='utf-8') as file:
             for row in field:
                 for cell in row:
                     file.write(cell.get_cell_type())
                 file.write('\n')
 
     @staticmethod
-    def from_file(filename: str):
+    def from_file(file_path: str):
         field: List[List[Cell]] = []
-        with open(filename, 'r', encoding='utf-8') as file:
+        with open(path.abspath(file_path), 'r', encoding='utf-8') as file:
             for i, row in enumerate(file):
                 field_row: List[Cell] = []
                 for j, symbol in enumerate(row):
