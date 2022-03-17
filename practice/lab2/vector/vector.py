21d1274f301c470732aa257268244d00f88185a0
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-03-17 19:10:32 +0300

    Переработал vector.py и его тесты

--- practice/lab2/vector/vector.py
+++ practice/lab2/vector/vector.py
@@ -1,5 +1,5 @@
 import sys
-from typing import List, Callable
+from typing import List, Callable, Optional
 
 
 def get_numbers_from_input() -> List[float]:
@@ -7,33 +7,35 @@ def get_numbers_from_input() -> List[float]:
     return [round(float(n), 3) for n in numbers]
 
 
-def multiply_numbers_mod_3_zero_to_even_avg(numbers: List[float]) -> List[float]:
+def calculate_result(numbers: List[float]) -> List[float]:
+    # Multiply each not even number to average value of all even numbers
     even_numbers: List[float] = []
     for number in numbers:
         if number % 2 == 0:
             even_numbers.append(number)
 
-    if not even_numbers:
-        return []
-
-    avg_even_numbers: float = sum(even_numbers) / len(even_numbers)
-    return [round(n * avg_even_numbers, 3) if n % 3 == 0 else n for n in numbers]
-
-
-def print_function_result(description: str, func: Callable, numbers: List[float]) -> None:
-    print('-' * 5, description, '-' * 5, sep='', end='\n\n')
-    result = func(numbers)
-    print(result.sort(), end='\n\n')
-    print('-' * (len(description) + 10), end='\n\n')
+    if even_numbers:
+        avg: float = sum(even_numbers) / len(even_numbers)
+        for i in range(len(numbers)):
+            if numbers[i] % 3 == 0:
+                numbers[i] *= avg
+    return numbers
 
 
-if __name__ == '__main__':
+def print_array(array: List[float]):
+    output: str = ','.join(str(n) for n in array)
+    print(output)
+
+
+def main():
     numbers = get_numbers_from_input()
-
     if len(numbers) == 0:
         print('List is empty')
         sys.exit(1)
 
-    print_function_result('multiply numbers mod 3 zero',
-                          multiply_numbers_mod_3_zero_to_even_avg,
-                          numbers)
+    result: List[float] = calculate_result(numbers)
+    print_array(result)
+
+
+if __name__ == '__main__':
+    main()
