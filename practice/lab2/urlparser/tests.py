619420083acb2691de5731e1e4b6595a55ce8dae
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-04-05 03:14:17 +0300

    Обновил тесты urlparser'а

--- practice/lab2/urlparser/tests.py
+++ practice/lab2/urlparser/tests.py
@@ -2,55 +2,123 @@ import pytest
 from urlparser import *
 
 
+def assert_parsed_urls(target: ParsedURL, excepted: ParsedURL) -> bool:
+    if target.protocol == excepted.protocol \
+            and target.host == excepted.host \
+            and target.port == excepted.port \
+            and target.document == excepted.document:
+        return True
+    return False
+
+
 def test_valid_url():
-    url = 'http://wwww.google.com:80/search/results'
+    url = 'http://www.google.com:80/search/results'
     parsed_url = parse_url(url)
-    assert validate_url(parsed_url) is True
+    excepted = ParsedURL(
+        'http',
+        'www.google.com',
+        80,
+        'search/results')
+    assert assert_parsed_urls(parsed_url, excepted)
 
 
 def test_url_without_docs():
-    url = 'http://wwww.google.com:80'
+    url = 'http://www.google.com:80'
     parsed_url = parse_url(url)
-    assert validate_url(parsed_url) is True
+    excepted = ParsedURL(
+        'http',
+        'www.google.com',
+        80,
+        ''
+        )
+    assert assert_parsed_urls(parsed_url, excepted)
 
 
 def test_url_without_port():
     url = 'https://www.vk.com'
     parsed_url = parse_url(url)
-    assert validate_url(parsed_url) is True
+    expected = ParsedURL(
+        'https',
+        'www.vk.com',
+        443,
+        ''
+    )
+    assert assert_parsed_urls(parsed_url, expected)
 
 
 def test_url_with_wrong_port():
     url = 'ftp://wwww.test.com:-100'
     parsed_url = parse_url(url)
-    assert validate_url(parsed_url) is False
+    assert parsed_url is None
 
 
 def test_url_with_zero_port():
     url = 'ftp://wwww.test.com:0'
     parsed_url = parse_url(url)
-    assert validate_url(parsed_url) is False
+    assert parsed_url is None
 
 
 def test_url_without_host():
     url = 'http:///docs/text.txt'
     parsed_url = parse_url(url)
-    assert validate_url(parsed_url) is False
+    assert parsed_url is None
 
 
 def test_url_with_empty_host():
     url = 'http://'
     parsed_url = parse_url(url)
-    assert validate_url(parsed_url) is False
+    assert parsed_url is None
 
 
 def test_url_with_wrong_protocol():
     url = 'httttp://www.google.com'
     parsed_url = parse_url(url)
-    assert validate_url(parsed_url) is False
+    assert parsed_url is None
 
 
 def test_empty_url():
     url = ''
     parsed_url = parse_url(url)
-    assert validate_url(parsed_url) is False
+    assert parsed_url is None
+
+
+def test_url_with_port_1():
+    url = 'ftp://www.download.com:1/docs/text.zip'
+    parsed_url = parse_url(url)
+    excepted = ParsedURL(
+        'ftp',
+        'www.download.com',
+        1,
+        'docs/text.zip'
+        )
+    assert assert_parsed_urls(parsed_url, excepted)
+
+
+def test_url_with_port_65355():
+    url = 'ftp://www.download.com:65355/docs/text.zip'
+    parsed_url = parse_url(url)
+    excepted = ParsedURL(
+        'ftp',
+        'www.download.com',
+        65355,
+        'docs/text.zip'
+        )
+    assert assert_parsed_urls(parsed_url, excepted)
+
+
+def test_url_with_001_port():
+    url = 'https://www.google.com:001/'
+    parsed_url = parse_url(url)
+    assert parsed_url is None
+
+
+def test_url_capitalize_protocol():
+    url = 'Https://www.vk.com/'
+    parsed_url = parse_url(url)
+    excepted = ParsedURL(
+        'https',
+        'www.vk.com',
+        443,
+        ''
+        )
+    assert assert_parsed_urls(parsed_url, excepted)
