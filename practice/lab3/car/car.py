db3b4061df81874233055e79cab2d8d4f221c1f0
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-04-14 16:01:21 +0300

    Добавил методы для переключения передач, указания скорости и свойства в Car

--- practice/lab3/car/car.py
+++ practice/lab3/car/car.py
@@ -1,26 +1,78 @@
-from gearbox import Gearbox
+from .gearbox import Gearbox
 
 
 class Car:
     _speed: float
     _gearbox: Gearbox
     _engine: bool
+    _direction: int
 
     def __init__(self):
         self._speed = 0
         self._gearbox = Gearbox()
         self._engine = False
+        self._direction = 0
 
-    def engine_on(self):
-        if self.engine:
+    def engine_on(self) -> bool:
+        if not self._engine:
+            self._engine = True
+        return True
+
+    def engine_off(self) -> bool:
+        if not self._engine:
             return True
 
-    def engine_off(self):
-        if not self.engine:
-            return True
-
-        if self.engine and self.gear == NeutralGear and self.speed == 0:
-            self.engine = False
+        if self._engine and self._gearbox.is_neutral_gear and self._speed == 0:
+            self._engine = False
             return True
 
         return False
+
+    def set_gear(self, new_gear: int) -> bool:
+        is_turned: bool = self._gearbox.change_gear(self._speed, new_gear)
+        if is_turned and self._gearbox.is_reverse_gear:
+            self._direction = -1
+
+        if not is_turned:
+            return False
+        return True
+
+    def set_speed(self, new_speed: float) -> True:
+        min_speed: float = self._gearbox.gear.min_speed
+        max_speed: float = self._gearbox.gear.max_speed
+
+        if min_speed <= new_speed <= max_speed:
+            self._speed = new_speed
+            return True
+        return False
+
+    @property
+    def is_turned_on(self) -> bool:
+        return self._engine
+
+    @property
+    def direction(self) -> str:
+        if self._direction == 1:
+            return 'Forward'
+        if self._direction == -1:
+            return 'Reverse'
+        return 'Stop'
+
+    @property
+    def speed(self) -> float:
+        return abs(self._speed)
+
+    @property
+    def gear(self) -> int:
+        return self._gearbox.gear.code
+
+    def info(self) -> str:
+        report: str = f"""
+        --- Car Info ---
+        Engine: {'On' if self.is_turned_on else 'Off'}
+        Direction: {self.direction}
+        Speed: {self.speed}
+        Gear: {self.gear}
+        --- End Car Info ---
+        """
+        return report
