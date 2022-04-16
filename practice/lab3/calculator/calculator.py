e06b5051d54ca865054a4285521814935eb17fcf
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-04-17 01:47:55 +0300

    Добавил методы get_variable_value, calculate_variable

--- practice/lab3/calculator/calculator.py
+++ practice/lab3/calculator/calculator.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 from function import Function
 
 
@@ -10,19 +10,58 @@ class Calculator:
         self._variables = {}
         self._functions = []
 
-    def add_variable(self, variable: str, value: float) -> None:
+    def _add_variable(self, variable: str, value: float) -> bool:
         if not self._has_variable(variable):
             self._variables[variable] = value
-            return
+            return True
 
         print(f'Can\'t add variable "{variable}" because same variable because it has already been declared')
+        return False
 
-    def update_variable(self, variable: str, new_value: float) -> None:
+    def update_variable(self, variable: str, new_value: float) -> bool:
         if not self._has_variable(variable):
             print(f'Can\'t update variable "{variable}" because variable was not declared')
-            return
+            return False
 
         self._variables[variable] = new_value
+        return True
+
+    def _calculate_variable(self, new_var_name: str,  variables: List[str], operations: List[str]) -> bool:
+        new_value: float = 0
+        for i, operation in enumerate(operations):
+            result = self.__perform_operation(operation, variables[i], variables[i+1])
+            if not result:
+                return False
+
+            new_value += result
+
+        self._add_variable(new_var_name, new_value)
+        return True
+
+    def __perform_operation(self, operation: str, first_var: str, second_var: str) -> Optional[float]:
+        first_value: float = self.get_variable_value(first_var)
+        second_value: float = self.get_variable_value(second_var)
+
+        if not first_value or not second_value:
+            return None
+
+        if operation == '+':
+            return first_value + second_value
+
+        if operation == '-':
+            return first_value - second_value
+
+        if operation == '*':
+            return first_value * second_value
+
+        return first_value / second_value
+
+    def get_variable_value(self, variable: str) -> Optional[float]:
+        if not self._has_variable(variable):
+            print(f'"{variable}" the variable was not declared')
+            return None
+
+        return self._variables[variable]
 
     def _has_variable(self, variable: str) -> bool:
         return variable in self._variables.keys()
