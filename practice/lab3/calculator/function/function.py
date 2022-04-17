870604f0fb75cf35d218e6db87068cbde5a12fd6
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-04-17 16:29:48 +0300

    Переименовал конструктор для Function и properties name, variables, operations

--- practice/lab3/calculator/function/function.py
+++ practice/lab3/calculator/function/function.py
@@ -1,6 +1,24 @@
-from typing import Dict, List
+from typing import List
 
 
 class Function:
-    _variables: Dict[str, float]
-    _actions: List[str]
+    _name: str
+    _variables: List[str]
+    _operations: List[str]
+
+    def __init__(self, name: str, variables: List[str], operations: List[str]) -> None:
+        self._name = name
+        self._variables = variables
+        self._operations = operations
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def variables(self):
+        return self._variables
+
+    @property
+    def operations(self):
+        return self._operations
