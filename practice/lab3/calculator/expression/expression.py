b8ac2b8974fd88cb13e9fb844349c87f68087353
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-05-12 18:58:10 +0300

    Добвавил класс Expression

---
+++ practice/lab3/calculator/expression/expression.py
@@ -0,0 +1,51 @@
+from typing import List, Optional
+
+
+class Expression:
+    _left_operand: Optional[str]
+    _right_operands: List[str]
+    _operation: Optional[str]
+    _possible_operators: List[str]
+
+    def __init__(self, left_operand: str, right_operands: Optional[List[str]] = None, operation: Optional[str] = None):
+        self._left_operand = left_operand
+        self._right_operands = right_operands
+        self._operation = operation
+
+    @property
+    def left_operand(self) -> str:
+        return self._left_operand
+
+    @property
+    def right_operands(self) -> List[str]:
+        return self._right_operands
+
+    @property
+    def operation(self) -> str:
+        return self._operation
+
+    @staticmethod
+    def parse_expr(expr: str):
+        equal_pos: int = expr.find('=')
+        if equal_pos == -1:
+            return Expression(expr)
+
+        left_operand: str = expr[:equal_pos].lstrip().rstrip()
+        right_expr: str = expr[equal_pos + 1:]
+        operator: str = Expression._get_operator_pos(right_expr)
+        right_operands: List[str] = right_expr.split(operator)
+
+        return Expression(left_operand, right_operands, operator)
+
+    @staticmethod
+    def _get_operators() -> List[str]:
+        operators: List[str] = ['+', '-', '*', '/']
+        return operators
+
+    @staticmethod
+    def _get_operator_pos(expr: str) -> Optional[str]:
+        for operator in Expression._get_operators():
+            if operator in expr:
+                return operator
+
+        return None
