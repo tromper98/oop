56dbebebdade2e343bfdd3f4389f824e2b77e859
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-05-16 00:36:50 +0300

    Добавил класс Point

---
+++ practice/lab4/shapes/base/point/point.py
@@ -0,0 +1,15 @@
+class Point:
+    _x: int
+    _y: int
+
+    def __init__(self, x: int, y: int):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self) -> int:
+        return self._x
+
+    @property
+    def y(self) -> int:
+        return self._y
