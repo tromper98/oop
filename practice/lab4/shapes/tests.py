6ca6046d7b39f3b10f8d8c9c38e5f0047a616020
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-05-19 19:31:50 +0300

    Добавил тесты для ShapeController

--- practice/lab4/shapes/tests.py
+++ practice/lab4/shapes/tests.py
@@ -1,9 +1,106 @@
+from typing import List
+
 import pytest
 
 from base.point import Point
 from base.exceptions import *
 from shapes import *
 
+from shapecontroller import ShapeController
+
+OUTPUT_STORAGE = []
+
+
+def is_equal_rectangle(target: Rectangle, expected: Rectangle) -> bool:
+    if target.get_left_top() != expected.get_left_top():
+        return False
+
+    if target.get_right_bottom() != expected.get_right_bottom():
+        return False
+
+    if target.get_fill_color() != expected.get_fill_color():
+        return False
+
+    if target.get_outline_color() != expected.get_outline_color():
+        return False
+
+    if target.get_perimeter() != expected.get_perimeter():
+        return False
+
+    if target.get_area() != expected.get_area():
+        return False
+
+    return True
+
+
+def is_equal_circle(target: Circle, expected: Circle):
+    if target.get_center() != expected.get_center():
+        return False
+
+    if target.get_radius() != expected.get_radius():
+        return False
+
+    if target.get_fill_color() != expected.get_fill_color():
+        return False
+
+    if target.get_outline_color() != expected.get_outline_color():
+        return False
+
+    if target.get_perimeter() != expected.get_perimeter():
+        return False
+
+    if target.get_area() != expected.get_area():
+        return False
+
+    return True
+
+
+def is_equal_triangle(target: Triangle, expected: Triangle) -> bool:
+    if target.get_vertex1() != expected.get_vertex1():
+        return False
+
+    if target.get_vertex2() != expected.get_vertex2():
+        return False
+
+    if target.get_vertex3() != expected.get_vertex3():
+        return False
+
+    if target.get_fill_color() != expected.get_fill_color():
+        return False
+
+    if target.get_outline_color() != expected.get_outline_color():
+        return False
+
+    if target.get_perimeter() != expected.get_perimeter():
+        return False
+
+    if target.get_area() != expected.get_area():
+        return False
+
+    return True
+
+
+def is_equal_line_segment(target: LineSegment, expected: LineSegment) -> bool:
+    if target.get_start_point() != expected.get_start_point():
+        return False
+
+    if target.get_end_point() != expected.get_end_point():
+        return False
+
+    if target.get_outline_color() != expected.get_outline_color():
+        return False
+
+    if target.get_perimeter() != expected.get_perimeter():
+        return False
+
+    if target.get_area() != expected.get_area():
+        return False
+
+    return True
+
+@pytest.mark.skip
+def test_print(string: str):
+    return OUTPUT_STORAGE.append(string)
 
 
 def test_circle_to_string():
@@ -103,5 +200,108 @@ def test_create_circle_with_max_outline_color_number():
     center = Point(0, 0)
     radius = 10
     fill_color = 2**32
-    with pytest.raises(InvalidFillColor):
-        Circle(center, radius, 10, fill_color)
+    Circle(center, radius, 10, fill_color)
+
+
+def test_create_rectangle_from_shape_controller():
+    controller: ShapeController = ShapeController(test_print)
+    params = ['0', '10', '100', '0', '15', '20']
+    controller.execute_command('rectangle', params)
+    expected = Rectangle(Point(0, 10), Point(100, 0), 15, 20)
+    assert is_equal_rectangle(controller._shapes[0], expected)
+
+
+def test_create_circle_from_shape_controller():
+    controller: ShapeController = ShapeController(test_print)
+    params = ['10', '-15', '100.5', '40', '100']
+    controller.execute_command('circle', params)
+    expected = Circle(Point(10, -15), 100.5, 40, 100)
+    assert is_equal_circle(controller._shapes[0], expected)
+
+
+def test_create_triangle_from_shape_controller():
+    controller: ShapeController = ShapeController(test_print)
+    params = ['0', '0', '10', '10', '20', '-50', '50', '30']
+    controller.execute_command('triangle', params)
+    expected = Triangle(Point(0, 0), Point(10, 10), Point(20, -50), 50, 30)
+    assert is_equal_triangle(controller._shapes[0], expected)
+
+
+def test_create_line_segment_from_shape_controller():
+    controller: ShapeController = ShapeController(test_print)
+    params = ['0', '0', '100', '200', '36']
+    controller.execute_command('line_segment', params)
+    expected = LineSegment(Point(0, 0), Point(100, 200), 36)
+    assert is_equal_line_segment(controller._shapes[0], expected)
+
+
+def test_fail_create_rectangle_with_not_enough_params():
+    OUTPUT_STORAGE.clear()
+    controller: ShapeController = ShapeController(test_print)
+    params = ['41', '42', '5', '3']
+    controller.execute_command('rectangle', params)
+    expected = 'Need 6 params to create rectangle but 4 params were given'
+    assert OUTPUT_STORAGE[0] == expected
+
+
+def test_fail_create_rectangle_with_invalid_params():
+    OUTPUT_STORAGE.clear()
+    controller: ShapeController = ShapeController(test_print)
+    params = ['41', '42', '5', '3', 'синий', 'зеленый']
+    controller.execute_command('rectangle', params)
+    expected = "invalid literal for int() with base 10: 'синий'"
+    assert OUTPUT_STORAGE[0].args[0] == expected
+
+
+def test_fail_create_circle_with_not_enough_params():
+    OUTPUT_STORAGE.clear()
+    controller: ShapeController = ShapeController(test_print)
+    params = ['100', '100', '2000']
+    controller.execute_command('circle', params)
+    expected = 'Need 5 params to create circle but 3 params were given'
+    assert OUTPUT_STORAGE[0] == expected
+
+
+def test_fail_create_circle_with_invalid_params():
+    OUTPUT_STORAGE.clear()
+    controller: ShapeController = ShapeController(test_print)
+    params = ['x1', 'x2', '5', '3', '10']
+    controller.execute_command('circle', params)
+    expected = "could not convert string to float: 'x1'"
+    assert OUTPUT_STORAGE[0].args[0] == expected
+
+
+def test_fail_create_triangle_with_not_enough_params():
+    OUTPUT_STORAGE.clear()
+    controller: ShapeController = ShapeController(test_print)
+    params = ['0', '0', '10', '19', '35', '49']
+    controller.execute_command('triangle', params)
+    expected = 'Need 8 params to create triangle but 6 params were given'
+    assert OUTPUT_STORAGE[0] == expected
+
+
+def test_fail_create_triangle_with_invalid_params():
+    OUTPUT_STORAGE.clear()
+    controller: ShapeController = ShapeController(test_print)
+    params = ['0', '0', 'vertex2', 'vertex2', '10', '10', '3', '8']
+    controller.execute_command('triangle', params)
+    expected = "could not convert string to float: 'vertex2'"
+    assert OUTPUT_STORAGE[0].args[0] == expected
+
+
+def test_fail_create_line_segment_with_not_enough_params():
+    OUTPUT_STORAGE.clear()
+    controller: ShapeController = ShapeController(test_print)
+    params = ['0', '0', '10', '19', '35', '49']
+    controller.execute_command('line_segment', params)
+    expected = f'Need 5 params to create line segment but {len(params)} params were given'
+    assert OUTPUT_STORAGE[0] == expected
+
+
+def test_fail_create_line_segment_with_invalid_params():
+    OUTPUT_STORAGE.clear()
+    controller: ShapeController = ShapeController(test_print)
+    params = ['0', '0', '10', '100', 'color']
+    controller.execute_command('line_segment', params)
+    expected = "invalid literal for int() with base 10: 'color'"
+    assert OUTPUT_STORAGE[0].args[0] == expected
