417ce1a5c7b1beca04343432cf3f1f9846655b68
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-05-25 02:08:41 +0300

    Обновил возврат данных из Calculator

--- practice/lab3/calculator/calculator.py
+++ practice/lab3/calculator/calculator.py
@@ -37,6 +37,15 @@ class Calculator:
         function = Function(operands, operation)
         self._functions[func_name] = function
 
+    def get_operand_by_name(self, name: str) -> Optional[float]:
+        if self._has_variable(name):
+            return self._variables[name]
+
+        if self._has_function(name):
+            return self.get_function_result(name)
+
+        return None
+
     def get_function_result(self, func_name: str) -> Optional[float]:
         if not self._has_function(func_name):
             raise FunctionNotFoundError(func_name)
@@ -45,30 +54,11 @@ class Calculator:
         result: Optional[float] = self._calculate_function_result(function)
         return result
 
-    def print_variable(self, variable: str) -> None:
-        if not self._has_variable(variable):
-            print(f'Variable "{variable}" does not exist')
-
-        result: Optional[float] = self.get_variable_value(variable)
-        print(result)
-
     def get_all_variables(self) -> List[Tuple[str, Optional[float]]]:
         return [(variable, value) for variable, value in self._variables.items()]
 
-    def print_function(self, func_name: str) -> None:
-        if not self._has_function(func_name):
-            print(f'Function  "{func_name}" does not exist')
-
-        function: Function = self._functions[func_name]
-        result: Optional[float] = self._calculate_function_result(function)
-        print(result)
-
-    def print_all_functions(self) -> None:
-        ordered_func_names: List[str] = sorted([func_name for func_name in self._functions.keys()])
-        for func_name in ordered_func_names:
-            function = self._functions[func_name]
-            result = self._calculate_function_result(function)
-            print(f'{func_name}: {result}')
+    def get_all_functions(self) -> List[Tuple[str, Optional[float]]]:
+        return [(func_name, self.get_function_result(func_name)) for func_name in self._functions.keys()]
 
     def _calculate_function_result(self, function: Function) -> Optional[float]:
         values: List[float] = []
@@ -132,26 +122,15 @@ class Calculator:
         if Calculator._is_number(operand):
             return float(operand)
 
-        if self._is_variable(operand):
+        if self._has_variable(operand):
             return self._variables[operand]
 
-        if self._is_function(operand):
+        if self._has_function(operand):
             function: Function = self._functions[operand]
             return self._calculate_function_result(function)
 
         raise OperandNotFoundError(operand)
 
-    def _is_variable(self, variable_name: str) -> bool:
-        if variable_name in [variable for variable in self._variables.keys()]:
-            return True
-
-        return False
-
-    def _is_function(self, function_name: str) -> bool:
-        if function_name in [function for function in self._functions.keys()]:
-            return True
-
-        return False
 
     @staticmethod
     def _is_number(number: Union[str, float]) -> bool:
