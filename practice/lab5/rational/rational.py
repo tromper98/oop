c50372e0c27b77916dc6f0bd8771f998e793a704
tromper98 <56966408+tromper98@users.noreply.github.com>
2022-06-05 00:12:48 +0300

    Добавил методы __pos__, __neg__ и тесты к ним

--- practice/lab5/rational/rational.py
+++ practice/lab5/rational/rational.py
@@ -1,22 +1,29 @@
 from __future__ import annotations
 
+from math import gcd
 from exceptions import *
 
 
 class Rational:
     _numerator: int
     _denominator: int
+    _sign: int
 
     def __init__(self, numerator: int = 0, denominator: int = 1):
         if denominator == 0:
             raise ZeroDenominatorError
 
-        self._numerator = numerator
-        self._denominator = denominator
+        if numerator < 0 or denominator < 0:
+            self._sign = -1
+        else:
+            self._sign = 1
+
+        self._numerator = abs(numerator)
+        self._denominator = abs(denominator)
 
     @property
     def numerator(self):
-        return self._numerator
+        return self._numerator * self._sign
 
     @property
     def denominator(self):
@@ -24,13 +31,24 @@ class Rational:
 
     @property
     def to_float(self):
-        return self._numerator / self._denominator
+        return self.numerator / self.denominator
 
-    @staticmethod
-    def _find_gcd(num1: int, num2: int):
-        while num1 != 0 and num2 != 0:
-            if num1 > num2:
-                num1 = num1 % num2
-            else:
-                num2 = num2 % num1
-        return num1 + num2
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Rational):
+            self_gcd = gcd(self.numerator, self.denominator)
+            other_gcd = gcd(other.numerator, other.denominator)
+
+            if self.numerator / self_gcd != other.numerator / other_gcd:
+                return False
+
+            if self.denominator / self_gcd != other.denominator / other_gcd:
+                return False
+
+            return True
+
+    def __pos__(self):
+        return Rational(self.numerator, self.denominator)
+
+    def __neg__(self):
+        return Rational(self.numerator * -1, self.denominator)
+
--- practice/lab5/rational/tests.py
+++ practice/lab5/rational/tests.py
@@ -9,13 +9,17 @@ def test_init_rational():
     rational2 = Rational(6)
     rational3 = Rational(2, 4)
     rational4 = Rational(0, 5)
+    rational5 = Rational(-1, 5)
+    rational6 = Rational(5, -6)
 
     assert rational1.numerator == 0 and rational1.denominator == 1
     assert rational2.numerator == 6 and rational2.denominator == 1
     assert rational3.numerator == 2 and rational3.denominator == 4
     assert rational4.numerator == 0 and rational4.denominator == 5
+    assert rational5.numerator == -1 and rational5.denominator == 5
+    assert rational6.numerator == -5 and rational6.denominator == 6
     with pytest.raises(ZeroDenominatorError):
-        rational5 = Rational(1, 0)
+        Rational(1, 0)
 
 
 def test_normalize_rationals():
@@ -31,15 +35,32 @@ def test_normalize_rationals():
     assert rational4 == rational5 == rational6
 
 
-def test_unary_plus_not_change_rational():
+def test_unary_plus_return_equal_rational():
     rational1 = Rational()
     rational2 = Rational(3, 5)
     rational3 = Rational(5, 25)
     rational4 = Rational(-1, 3)
     rational5 = Rational(-8, 64)
 
-    assert + rational1 == rational1
-    assert + rational2 == rational2
-    assert + rational3 == rational3
-    assert + rational4 == rational4
-    assert + rational5 == rational5
+    assert +rational1 == rational1
+    assert +rational2 == rational2
+    assert +rational3 == rational3
+    assert +rational4 == rational4
+    assert +rational5 == rational5
+
+
+def test_unary_minus_return_opposite_rational():
+    rational1 = Rational()
+    rational2 = Rational(1, 5)
+    rational3 = Rational(-1, 8)
+    rational4 = Rational(0, 5)
+
+    expected_rational1 = Rational()
+    expected_rational2 = Rational(-1, 5)
+    expected_rational3 = Rational(1, 8)
+    expected_rational4 = Rational(0, 5)
+
+    assert -rational1 == expected_rational1
+    assert -rational2 == expected_rational2
+    assert -rational3 == expected_rational3
+    assert -rational4 == expected_rational4
